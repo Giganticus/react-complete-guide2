ea38cd08bcb84dee35aec1a313b34ee385f6d8cf
Christopher Booth <christopher.booth@juliusbaer.com>
2023-09-12 12:25:09 +0200

    Slight tweak

--- src/components/Concepts/Concepts.js
+++ src/components/Concepts/Concepts.js
@@ -3,11 +3,12 @@ import Card from "../UI/Card";
 import Header from "./Header";
 import Concept from "./Concept";
 
-const Concepts = (props) => {
+const Concepts = ({concepts}) => {
     return (<Card>
         <Header></Header>
         <ul id="concepts">
-            {props.concepts.map((item) => Concept({image: item.image, description: item.description, title: item.title}))}
+            {concepts.map((item) => 
+                Concept({image: item.image, description: item.description, title: item.title}))}
         </ul>
     </Card>);
 }
